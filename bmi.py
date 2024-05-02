534efb5d3d2a342fa2c579bee47c75a5d8f06169
Hareneraj <HARENERAJ12.23@ichat.sp.edu.sg>
2024-05-02 14:52:05 +0800

    3 return values

--- bmi.py
+++ bmi.py
@@ -6,9 +6,12 @@ def calculate_bmi(height,weight):
 
     if bmi<18.5:
         print("bmi_range= under weight")
+        return -1
     elif bmi >18.5 and bmi <=25.0:
         print("bmi_range= normal weight")
+        return 0
     elif bmi>25.0:
         print("bmi_range= over weight")
+        return 1
        
 calculate_bmi(weight=57,height=1.73)
